7a283a6bc7f3eab7d878928f6e8469f0fb732a38
tomkp <tom@tomkp.com>
2016-03-21 20:10:27 +0000

    ...

--- src/sections/footer/Footer.js
+++ src/sections/footer/Footer.js
@@ -2,5 +2,9 @@ import React from 'react';
 import './footer.scss';
 
 export default () => {
-    return <div className="footer">Footer</div>
+    return <div className="footer">
+        <span className="atr" title="ATR (Answer To Reset)">3b6d00000031c071d66419160100849000</span>
+        <span className="device-status deactivated"></span>
+        <span className="card-status removed"></span>
+    </div>
 };
