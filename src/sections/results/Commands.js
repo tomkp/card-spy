d0fdca33870324bf4de41167791e40b4f44d92e0
tomkp <tom@tomkp.com>
2016-03-27 08:50:55 +0100

    add split-pane

--- src/sections/results/Commands.js
+++ src/sections/results/Commands.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import CommandResponse from './CommandResponse';
+import SplitPane from 'react-split-pane';
 import './commands.scss';
 
 
 
 export default ({commands}) => {
     return (
-        <div className="commands">
-            { commands.map((result, key) => {
-                return <CommandResponse key={key}
-                                command={result.command}
-                                ok={result.ok}
-                                meaning={result.meaning}
-                                response={result.response} />
-            })
-            }
-        </div>
+        <SplitPane split="vertical" minSize="50" defaultSize="100">
+            <div className="commands">
+                { commands.map((result, key) => {
+                    return <CommandResponse key={key}
+                                            command={result.command}
+                                            ok={result.ok}
+                                            meaning={result.meaning}
+                                            response={result.response} />
+                })
+                }
+            </div>
+            <div></div>
+        </SplitPane>
+
     )
 };
