c0920472f4c11929e40f21b303fc32b6402c2b04
tomkp <tom@tomkp.com>
2016-03-31 23:58:03 +0100

    ...

--- src/sections/footer/Footer.js
+++ src/sections/footer/Footer.js
@@ -4,9 +4,9 @@ import './footer.scss';
 
 export default ({device, card}) => {
     return <div className="footer">
-        <span className="reader">{device?device.name:''}</span>
-        <span className="atr" title="ATR (Answer To Reset)">{card?card:''}</span>
         <Status name="device-status" status={device?'activated':'deactivated'} title={device?'Device activated':'Device deactivated'} />
         <Status name="card-status" status={card?'inserted':'removed'} title={card?'Card inserted':'Card removed'} />
+        <span className="reader">{device?device.name:''}</span>
+        <span className="atr" title="ATR (Answer To Reset)">{card?card:''}</span>
     </div>
 };
