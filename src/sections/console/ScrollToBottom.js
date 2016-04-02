9306dd901b377ede268082cf84f46dac2f0c5beb
tomkp <tom@tomkp.com>
2016-04-02 19:15:41 +0100

    ...

--- src/sections/console/ScrollToBottom.js
+++ src/sections/console/ScrollToBottom.js
@@ -4,18 +4,12 @@ import './scroll-to-bottom.scss';
 
 export default class extends React.Component {
 
-    constructor() {
-        super();
-    }
-
     componentWillUpdate() {
-        console.log(`ScrollToBottom.componentWillUpdate`);
         var node = ReactDOM.findDOMNode(this);
         this.shouldScrollBottom = node.scrollTop + node.offsetHeight === node.scrollHeight;
     };
 
     componentDidUpdate() {
-        console.log(`ScrollToBottom.componentDidUpdate`);
         if (this.shouldScrollBottom) {
             var node = ReactDOM.findDOMNode(this);
             node.scrollTop = node.scrollHeight
@@ -23,7 +17,6 @@ export default class extends React.Component {
     };
 
     render() {
-        //console.log(`ScrollToBottom.render`);
         return <div className={`scroll-to-bottom ${this.props.className}`}>{this.props.children}</div>;
     }
 };
