e988000d633129e0fb2eaf3391cbca78790492c2
tomkp <tom@tomkp.com>
2016-04-02 22:08:05 +0100

    refactor / tidy up

--- card-explorer.js
+++ card-explorer.js
@@ -2,26 +2,18 @@
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
-
 const electron = require('electron');
-
 const smartcard = require('smartcard');
 const tlv = require('tlv');
 const hexify = require('hexify');
-
 
 if (process.env.NODE_ENV === 'development') {
     require('electron-debug')();
 }
 
-
-// Module to control application life.
 const app = electron.app;
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 const Devices = smartcard.Devices;
@@ -31,99 +23,105 @@ const CommandApdu = smartcard.CommandApdu;
 const ipcMain = require('electron').ipcMain;
 
 
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+
+
 function createWindow() {
-    // Create the browser window.
     mainWindow = new BrowserWindow({width: 640, height: 740, icon: './tomkp.png', title: 'Card Explorer'});
-
-    // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
-
-    // Open the DevTools.
     let webContents = mainWindow.webContents;
-
-    //webContents.openDevTools();
     if (process.env.NODE_ENV === 'development') {
         webContents.openDevTools();
     }
 
-    // Emitted when the window is closed.
     mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null;
     });
 
-
-    webContents.on('did-finish-load', onLoaded);
-
-
-    function onLoaded() {
-
-        const devices = new Devices();
-
-        devices.on('device-activated', function (event) {
-
-            const currentDevices = event.devices;
-            let device = event.device;
-            console.log(`Device '${device}' activated, devices: ${currentDevices}`);
-            for (let prop in currentDevices) {
-                console.log("Devices: " + currentDevices[prop]);
-            }
-
-            webContents.send('device-activated', {device: device, devices: currentDevices});
-
-            device.on('card-inserted', function (event) {
-                let card = event.card;
-                console.log(`Card '${event.card}' inserted into '${event.device}'`);
-                webContents.send('card-inserted', {atr: event.card.getAtr(), device: device.toString()});
-
-                card.on('command-issued', function (event) {
-                    console.log(`Command '${event.command}' issued to '${event.card}' `);
-                    webContents.send('command-issued', {command: event.command.toString(), atr: event.card.getAtr()});
-                });
-
-                card.on('response-received', function (event) {
-                    console.log(`Response '${event.response}' received from '${event.card}' in response to '${event.command}'`);
-                    webContents.send('response-received', {
-                        command: event.command.toString(),
-                        response: event.response.toString(),
-                        ok: event.response.isOk(),
-                        meaning: event.response.meaning(),
-                        atr: event.card.getAtr()
-                    });
-                });
-
-                const application = new Iso7816Application(card);
-
-                application.on('application-selected', function (event) {
-                    console.log(`Application Selected ${event.command} ${event.response}`);
-                    webContents.send('application-selected', {application: event.application});
-                });
-
-                ipcMain.on('repl', function (event, message) {
-                    console.log(`REPL ${message}`);
-                    application.issueCommand(new CommandApdu({bytes: hexify.toByteArray(message)}))
-                });
-
-                ipcMain.on('interrogate', function (event, message) {
-                    console.log(`interrogate`);
-                    selectPse(webContents, application);
-                });
+    webContents.on('did-finish-load', function() {
+        onLoaded(webContents)
+    });
+}
 
 
+function onLoaded(webContents) {
+
+    const devices = new Devices();
+
+    devices.on('device-activated', function (event) {
+
+        const currentDevices = event.devices;
+        let device = event.device;
+        console.log(`Device '${device}' activated, devices: ${currentDevices}`);
+        for (let prop in currentDevices) {
+            console.log("Devices: " + currentDevices[prop]);
+        }
+
+        webContents.send('device-activated', {device: device, devices: currentDevices});
+
+        device.on('card-inserted', function (event) {
+            let card = event.card;
+            console.log(`Card '${event.card}' inserted into '${event.device}'`);
+            webContents.send('card-inserted', {atr: event.card.getAtr(), device: device.toString()});
+
+            card.on('command-issued', function (event) {
+                console.log(`Command '${event.command}' issued to '${event.card}' `);
+                webContents.send('command-issued', {command: event.command.toString(), atr: event.card.getAtr()});
             });
-            device.on('card-removed', function (event) {
-                console.log(`Card removed from '${event.name}' `);
-                webContents.send('card-removed', event);
-            });
-        });
 
-        devices.on('device-deactivated', function (event) {
-            console.log(`Device '${event.reader.name}' deactivated, devices: ${devices.listDevices()}`);
-            webContents.send('device-deactivated', event);
+            card.on('response-received', function (event) {
+                console.log(`Response '${event.response}' received from '${event.card}' in response to '${event.command}'`);
+                webContents.send('response-received', {
+                    command: event.command.toString(),
+                    response: event.response.toString(),
+                    ok: event.response.isOk(),
+                    meaning: event.response.meaning(),
+                    atr: event.card.getAtr()
+                });
+            });
+
+            const application = new Iso7816Application(card);
+
+            application.on('application-selected', function (event) {
+                console.log(`Application Selected ${event.command} ${event.response}`);
+                webContents.send('application-selected', {application: event.application});
+            });
+
+            ipcMain.on('repl', function (event, message) {
+                console.log(`REPL ${message}`);
+                application.issueCommand(new CommandApdu({bytes: hexify.toByteArray(message)}))
+            });
+
+            ipcMain.on('interrogate', function (event, message) {
+                console.log(`interrogate`);
+                selectPse(webContents, application);
+            });
+
+
         });
-    }
+        device.on('card-removed', function (event) {
+            console.log(`Card removed from '${event.name}' `);
+            webContents.send('card-removed', event);
+        });
+    });
+
+    devices.on('device-deactivated', function (event) {
+        console.log(`Device '${event.reader.name}' deactivated, devices: ${devices.listDevices()}`);
+        webContents.send('device-deactivated', event);
+    });
 }
 
 function selectPse(webContents, application) {
@@ -211,27 +209,6 @@ function filterApplicationIds(recordResponses) {
 }
 
 
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow();
-    }
-});
 
 
 let emvTags = {
