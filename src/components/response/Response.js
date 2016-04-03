54ac712f7d0fb8a7cdbcef5bf1066954efb48cff
tomkp <tom@tomkp.com>
2016-04-03 22:47:57 +0100

    ...

--- src/components/response/Response.js
+++ src/components/response/Response.js
@@ -7,7 +7,7 @@ export default ({response, ok, meaning}) => {
     return (
         <div className={'response ' + (ok?'ok':'error')} title={`Response APDU`}>
             {response}
-            {ok?<Tlv data={response} />:
+            {ok?<div className="tlv-wrapper"><Tlv data={response} /></div>:
                 <span className="meaning">{meaning}</span>
             }
         </div>
