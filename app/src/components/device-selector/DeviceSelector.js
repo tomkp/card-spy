d3e6c518a50ccbcb6500330736ee9b9836057481
tomkp <tom@tomkp.com>
2016-04-16 21:54:43 +0100

    device selection

--- app/src/components/device-selector/DeviceSelector.js
+++ app/src/components/device-selector/DeviceSelector.js
@@ -5,17 +5,12 @@ import Indicator from '../indicator/Indicator';
 
 const Device = ({dx, device, active, onSelectDevice}) => {
 
-    const clicked = (e) => {
-        onSelectDevice(device);
-    };
-
     const cx = dx[device] ? dx[device].card : null;
-    const d = `${device}`;
     return (
-        <div className={`device ${active?'active':''}`} onClick={clicked}>
+        <div className={`device ${active?'active':''}`} onClick={() => onSelectDevice(device)}>
             <Indicator name="device-status" status={device?'activated':'deactivated'} title={device?'Device activated':'Device deactivated'} />
             <Indicator name="card-status" status={cx?'inserted':'removed'} title={cx?'Card inserted':'Card removed'} />
-            {d}
+            <div className={`selected-device`}>{device}</div>
         </div>
     )
 };
@@ -40,7 +35,7 @@ export default class DeviceSelector extends React.Component {
     }
 
     render() {
-        console.log(`DeviceSelector.render: '${this.state.open}' '${this.props.currentDevice}' '${this.props.devices}' '${this.props.onSelectDevice}'`);
+        //console.log(`DeviceSelector.render: '${this.state.open}' '${this.props.currentDevice}' '${this.props.devices}' '${this.props.onSelectDevice}'`);
 
         const children = this.props.devices.map((device, index) => {
             //console.log(`device '${device}' '${currentDevice}' ${currentDevice === device} ${onSelectDevice}`);
